96c17b3b66738232d94014e868f459f1f4a2a397
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-09 08:57:37 -0400

    first

---
+++ js/style.js
@@ -0,0 +1,52 @@
+function add(x, y) {
+    return x+y;
+}
+console.log(add(1, 2)); 
+
+function adda(x, y,z) {
+    return x+y+z;
+}
+console.log(adda(1, 2,3)); 
+
+function addb(x, y, z, n) {
+    return x+y+z+n;
+}
+console.log(addb(1, 2, 3, 4)); 
+
+
+var cats = {
+    'color' : 'black',
+    'fixed' : 'yes',
+    'friendly': ' yes'
+} ;
+
+    cats.fur = 'long';
+
+    cats.fixed = 'no';
+
+    console.log(cats.color)
+
+var dogs = {
+    'tail': 'long',
+    'color' : 'white',
+    'friendly': 'yes'   
+ 
+};
+    dogs.bark = "bow-wow";
+
+    dogs.tail = 'short';
+
+    console.log(dogs['color'])
+
+    var birds ={
+    'sings': 'yes',
+    'flys' : 'no',
+    'caged' : 'yes'
+};
+    birds[talks] = 'yes';
+
+    birds[caged] = 'no';
+
+    console.log(birds.caged)
+
+
